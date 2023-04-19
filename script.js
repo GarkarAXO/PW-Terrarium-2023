4ed9c87f85b7956bf594d4792dea01c8620f6576
GarkarAXO <99782340+GarkarAXO@users.noreply.github.com>
2023-04-19 18:30:27 +0000

    Controlando elementos

---
+++ script.js
@@ -0,0 +1,51 @@
+let plant1 = document.getElementById("plant1");
+ plant1.addEventListener("click", function(){
+    let name = prompt("hola, ¿cual es tu nombre?");
+    if (name !== "")
+    alert( `${name} please, SAVE THE PLANET!!! 🌱`);
+ });
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("plant2");
+// document.getElementById("plant3");
+// document.getElementById("plant4");
+// document.getElementById("plant5");
+// document.getElementById("plant6");
+// document.getElementById("plant7");
+// document.getElementById("plant8");
+// document.getElementById("plant9");
+// document.getElementById("plant10");
+// document.getElementById("plant11");
+// document.getElementById("plant12");
+// document.getElementById("plant13");
+// document.getElementById("plant14");
+
