73a9e01165f1fb84e46c629310e7316039366468
GarkarAXO <99782340+GarkarAXO@users.noreply.github.com>
2023-04-21 18:08:10 +0000

    Terrario completo ⭐🌱🫙👍👌

--- script.js
+++ script.js
@@ -17,15 +17,54 @@ function dragElement(plantElement) {
     // Variables para establecer posiciones iniciales y finales
     let pos1 = 0,
     pos2 = 0,
+    //pos 3 graba posicion en x de mouse al momento
+    //de hacer onpointerdown a un elemento
+    //pos4 graba posicion
     pos3 = 0,
     pos4 = 0;
     //registrar un evento
     plantElement.onpointerdown =  pointerDrag
 
-    function pointerDrag(event){
-        //Previene todo comportamiento
-        //que tenga el navegador por defecto
+    function pointerDrag( event ){
+        // Previene todo comportamiento
+        // que tenga por defecto
+        // el evento en cuestion en
+        // mi documento
         event.preventDefault();
-        console.log("+On pointer down");
+        // Vamos a guardar la posicion inicial
+        // que se tiene al momento de hacer
+        // el pointerDrag
+        pos3 = event.clientX;
+        pos4 = event.clientY;
+
+        // Registrando handlers para eventos
+        // de arrastre
+        document.onpointermove = elementDrag;
+        // Registrando handler para evento
+        // de liberación del boton
+        document.onpointerup = stopElementDrag;
+
+        //creando las funciones
+        function elementDrag(event){
+            pos1 = pos3 - event.clientX;
+            pos2 = pos4 - event.clientY;
+            //actualizamos posiciones originales
+            pos3 = event.clientX;
+            pos4 = event.clientY;
+            // Mover la imagen con las posiciones
+            // con las posiciones calculadas
+            // de modo que la imagen quede en
+            // la nueva posicion del mouse
+            plantElement.style.left = `${plantElement.offsetLeft - pos1}px`;
+            plantElement.style.top = `${plantElement.offsetTop - pos2}px`;
+        }
+        function stopElementDrag(event){
+            // Eliminando handler para evento
+            // de liberación del boton
+            document.onpointerup = null;
+            // Eliminando handler para evento
+            // de arrastre
+            document.onpointermove = null;
+        }
     }
 }
